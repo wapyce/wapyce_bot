3357e61d8623362754aca96fe631da4634161f5e
Carlson Santana Cruz <carlsonsantana@gmail.com>
2018-10-23 01:26:08 -0300

    :sparkles: Submit issues of pages and finish validation

---
+++ submit_validation.js
@@ -0,0 +1,205 @@
+#!/usr/bin/env node
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const request = require('request');
+
+const config = require('./config');
+
+const SUBMIT_PAGES_PATH = (
+    config.wapyce_url_address
+    + 'api/v1/validation/pages/'
+);
+const SUBMIT_ISSUES_PATH = (
+    config.wapyce_url_address
+    + 'api/v1/accessibility/issues/'
+);
+const FINISH_VALIDATION_PATH = (
+    config.wapyce_url_address +
+    'api/v1/validation/validations/%s/finish_validation/'
+);
+const HEADERS = {
+    'Authorization': 'Token ' + config.user_token,
+    'Content-Type': 'application/json'
+}
+
+var argv = require('yargs')
+    .usage('Usage: $0 -o site.json')
+    .alias('i', 'validation-file')
+    .default('i', config.default_validation_file)
+    .alias('r', 'request-validation-file')
+    .default('r', config.default_request_validation_file)
+    .alias('p', 'pages-file')
+    .default('p', config.default_pages_file)
+    .argv;
+var validationFile = argv.i;
+var requestValidationFile = argv.r;
+var pagesFile = argv.p;
+
+var validationContent = fs.readFileSync(validationFile, 'utf8');
+var completeValidations = JSON.parse(validationContent);
+var completeIssues = [];
+
+var wapyceValidation = JSON.parse(
+    fs.readFileSync(requestValidationFile, 'utf8')
+);
+var wapyceValidationUUID = wapyceValidation['uuid'];
+
+for (var i = 0, length = completeValidations.length; i < length; i++) {
+    completeIssues = completeIssues.concat(completeValidations[i]['issues']);
+}
+
+function finishValidation() {
+    var options = {
+        method: 'PUT',
+        url: FINISH_VALIDATION_PATH.replace('%s', wapyceValidationUUID),
+        headers: HEADERS
+    };
+    request(options, function(error, response, body) {
+        if (error) {
+            throw error;
+        }
+
+        if (response.statusCode === 200) {
+            var validationDirectory = path.join(
+                config.directory_validations,
+                wapyceValidationUUID
+            );
+
+            if (!fs.existsSync(config.directory_validations)) {
+                fs.mkdirSync(config.directory_validations);
+            }
+            if (!fs.existsSync(validationDirectory)) {
+                fs.mkdirSync(validationDirectory);
+            }
+
+            fs.renameSync(
+                requestValidationFile,
+                path.join(
+                    validationDirectory,
+                    path.basename(config.default_request_validation_file)
+                )
+            );
+            fs.renameSync(
+                pagesFile,
+                path.join(
+                    validationDirectory,
+                    path.basename(config.default_pages_file)
+                )
+            );
+            fs.renameSync(
+                inputFile,
+                path.join(
+                    validationDirectory,
+                    path.basename(config.default_validation_file)
+                )
+            );
+
+            console.log('Validation finished.');
+        } else {
+            console.log('Validation not finished.');
+        }
+    });
+}
+
+function submitIssues(issues) {
+    if (issues.length === 0) {
+        finishValidation();
+        return ;
+    }
+
+    var issue = issues.shift();
+    if (!issue.hasOwnProperty('uuid')) {
+        var options = {
+            method: 'POST',
+            url: SUBMIT_ISSUES_PATH,
+            headers: HEADERS,
+            form: {
+                page: issue['page'],
+                code: issue['code'],
+                context: issue['context'],
+                message: issue['message'],
+                selector: issue['selector'],
+                issue_type: issue['typeCode']
+            }
+        };
+        request(options, function(error, response, body) {
+            if (error) {
+                throw error;
+            }
+
+            if (response.statusCode === 201) {
+                var wapyceIssue = JSON.parse(body);
+
+                issue['uuid'] = wapyceIssue['uuid'];
+
+                fs.writeFileSync(
+                    inputFile,
+                    JSON.stringify(completeValidations),
+                    'utf8'
+                );
+
+                console.log('Issue of page submitted.');
+            } else {
+                console.log('Issue of page not submitted.');
+            }
+            submitIssues(issues);
+        });
+    } else {
+        submitIssues(issues);
+    }
+}
+
+function submitPages(validations, index) {
+    if (validations.length === 0) {
+        submitIssues(completeIssues);
+        return ;
+    }
+
+    var validation = validations.shift();
+
+    if (!validation.hasOwnProperty('uuid')) {
+        var options = {
+            method: 'POST',
+            url: SUBMIT_PAGES_PATH,
+            headers: HEADERS,
+            form: {
+                validation_site: wapyceValidationUUID,
+                page_url: validation['pageUrl']
+            }
+        };
+
+        request(options, function(error, response, body) {
+            if (error) {
+                throw error;
+            }
+
+            if (response.statusCode === 201) {
+                var wapycePage = JSON.parse(body);
+
+                completeValidations[index]['uuid'] = wapycePage['uuid'];
+                var issues = completeValidations[index]['issues']
+                for (var i = 0, length = issues.length; i < length; i++) {
+                    issues[i]['page'] = wapycePage['uuid'];
+                }
+
+                fs.writeFileSync(
+                    inputFile,
+                    JSON.stringify(completeValidations),
+                    'utf8'
+                );
+
+                console.log('Validated page submitted.');
+            } else {
+                console.log('Validated page not submitted.');
+            }
+            submitPages(validations, index + 1);
+        });
+    } else {
+        submitPages(validations, index + 1);
+    }
+}
+
+submitPages(JSON.parse(validationContent), 0);
