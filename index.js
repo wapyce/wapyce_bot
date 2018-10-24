39a66f6a5685ae8b1e9c4ba311bf60363dffd140
Carlson Santana Cruz <carlsonsantana@gmail.com>
2018-10-24 14:14:40 -0300

    :sparkles: Allow that user repeat the request and submit of validations

--- index.js
+++ index.js
@@ -10,25 +10,43 @@ const validate_pages = require('./validate_pages');
 const submit_validation = require('./submit_validation');
 
 
-request_validation.run(
-    config.default_request_validation_file
-).then(function() {
-    var data = fs.readFileSync(config.default_request_validation_file, 'utf8');
-    var site = JSON.parse(data);
-
-    collect_urls_site.run(
-        site['site'],
-        config.default_pages_file
+function run(loop) {
+    request_validation.run(
+        config.default_request_validation_file
     ).then(function() {
-        validate_pages.run(
-            config.default_pages_file,
-            config.default_validation_file
+        var data = fs.readFileSync(
+            config.default_request_validation_file,
+            'utf8'
+        );
+        var site = JSON.parse(data);
+
+        collect_urls_site.run(
+            site['site'],
+            config.default_pages_file
         ).then(function() {
-            submit_validation.run(
-                config.default_validation_file,
-                config.default_request_validation_file,
-                config.default_pages_file
-            );
-        });
-    })
-});
+            validate_pages.run(
+                config.default_pages_file,
+                config.default_validation_file
+            ).then(function() {
+                submit_validation.run(
+                    config.default_validation_file,
+                    config.default_request_validation_file,
+                    config.default_pages_file
+                ).then(function() {
+                    if (loop) {
+                        run(loop);
+                    }
+                });
+            });
+        })
+    });
+}
+
+var argv = require('yargs')
+    .usage('Usage: $0 [OPTIONS]')
+    .alias('l', 'loop')
+    .argv;
+var loop = argv.l;
+
+
+run(loop);
